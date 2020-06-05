5496450dc26c270935caaa915dea840db80e4b46
Sachin37-ui <64476333+Sachin37-ui@users.noreply.github.com>
2020-06-05 18:55:24 +0530

    Update success

--- model_success.py
+++ model_success.py
@@ -1,6 +1,6 @@
 import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)
-s=starttls()
+s.starttls()
 s.login("tendlysachin8@gmail.com","Sarthak2@15")
 message="training successful"
 s.sendmail("tendlysachin8@gmail.com","sm026552@gmail.com",message)
