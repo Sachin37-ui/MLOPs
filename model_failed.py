3cba9e3b5589cfd929784fc7f0f76f07ef745740
Sachin37-ui <64476333+Sachin37-ui@users.noreply.github.com>
2020-06-06 19:12:58 +0530

    model fail

--- model_failed.py
+++ model_failed.py
@@ -2,7 +2,7 @@ import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
 s.login("tendlysachin8@gmail.com","Sarthak2@15")
-message="training failed"
+message="model training is failed"
 s.sendmail("tendlysachin8@gmail.com","sm026552@gmail.com",message)
 print("mail sent")
 s.quit()
